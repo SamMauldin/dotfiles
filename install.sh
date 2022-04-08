50e0faf2b114922beed049117705532e4708a65f
Sam Mauldin <sam@mauldin.me>
2022-04-08 10:24:47 -0500

    Extract nvim

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eou pipefail
+
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply git@git.sr.ht:~sam_mauldin/dotfiles
 
 mkdir -p ~/bin
@@ -7,3 +9,8 @@ mkdir -p ~/bin
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv ./nvim.appimage ~/bin/nvim
+cd ~/bin
+./nvim --appimage-extract
+mv squashfs-root nvim-extracted
+rm nvim
+ln -s $HOME/bin/nvim-extracted/usr/bin/nvim $HOME/bin/nvim
